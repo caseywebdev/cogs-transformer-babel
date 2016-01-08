28200db65707f108325290cdea22f630a0a4c7a9
Casey Foster <c@sey.me>
2016-01-08 11:57:58 -0600

    Add codeFrame to error if applicable

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ module.exports = function (file, options, cb) {
   var source = file.buffer.toString();
   options = _.extend({filename: file.path}, options);
   try { source = babel.transform(source, options).code + '\n'; }
-  catch (er) { return cb(er); }
+  catch (er) {
+    if (er.codeFrame) er.message += '\n' + er.codeFrame;
+    return cb(er);
+  }
   cb(null, {buffer: new Buffer(source)});
 };
