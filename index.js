8206e46e51e53cf56e01f73ecb6d9814ed5de872
Casey Foster <c@sey.me>
2019-11-15 11:50:30 -0600

    Use transformAsync API, 4.0.1

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const {promisify} = require('util');
 const _ = require('underscore');
-const babel = require('@babel/core');
+const { transformAsync } = require('@babel/core');
 
-const transform = promisify(babel.transform);
-
-module.exports = async ({file: {buffer, path}, options}) => {
+module.exports = async ({ file: { buffer, path }, options }) => {
   try {
     const source = buffer.toString();
-    options = _.extend({filename: path}, options);
-    const {code} = await transform(source, options);
-    return {buffer: Buffer.from(`${code}\n`)};
+    options = _.extend({ filename: path }, options);
+    const { code } = await transformAsync(source, options);
+    return { buffer: Buffer.from(`${code}\n`) };
   } catch (er) {
     if (er.codeFrame) er.message += `\n${er.codeFrame}`;
     throw er;
--- test/output.js
+++ test/output.js
@@ -3,18 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
 var _jquery = _interopRequireDefault(require("jquery"));
 
 var _react = _interopRequireDefault(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var app = (0, _jquery.default)(function () {
-  return _react.default.createElement("div", {
+var app = (0, _jquery["default"])(function () {
+  return _react["default"].createElement("div", {
     id: "foo"
   });
 });
 var _default = app;
-exports.default = _default;
+exports["default"] = _default;
